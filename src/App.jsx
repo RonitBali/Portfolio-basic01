2c815b953c4956e76e7d5f4ed14a4f3064dbc3ad
RONIT <balironit@gmail.com>
2024-09-10 23:50:35 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-
+import React from 'react'
 import './App.css'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/expericence/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/footer/Footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-     
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   )
 }
